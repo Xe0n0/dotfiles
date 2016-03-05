a7976143ad8ef258654f7384b855e645c0e95dd7
Haotian Wu <wu.haotian@me.com>
2016-03-05 00:58:58 -0500

    Add git config script

---
+++ config.sh
@@ -0,0 +1,5 @@
+config () {
+  scripts/$1.cfg.sh
+}
+
+config git
---
+++ scripts/git.cfg.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Haotian Wu"
+git config --global user.email wu.haotian@me.com
+
+ln -s -F `pwd`/gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
